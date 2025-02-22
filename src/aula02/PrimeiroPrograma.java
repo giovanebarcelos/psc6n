417de3b3eb41afad369602df5d19562b90248beb
Giovane Barcelos <giovanebarcelos@gmail.com>
2025-02-21 21:23:11 -0300

    feat: Meu primeiro programa com entrada e saída de dados, bem como uso de métodos

---
+++ src/aula02/PrimeiroPrograma.java
@@ -0,0 +1,39 @@
+package aula02;
+
+import java.util.Scanner;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.printf("Meu Primeiro Programa");
+
+        String nome;
+        int idade;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("\nNome: ");
+        nome = scanner.nextLine();
+        System.out.printf("Idade: ");
+        idade = scanner.nextInt();
+
+        System.out.printf("\nNome: " + nome+
+                          "; idade: " + idade);
+
+        imprimirDobroIdade(idade);
+
+        int novaIdade = multiplicarIdade(idade, 2);
+        System.out.printf("\nNova Idade = " + novaIdade);
+
+    }
+
+    private static int multiplicarIdade(int idade, int multiplicador) {
+        int novaIdade = idade * multiplicador;
+
+        return novaIdade;
+    }
+
+    private static void imprimirDobroIdade(int idade) {
+        int dobroIdade = idade * 2;
+
+        System.out.printf("\nO dobro da idade é " + dobroIdade);
+    }
+}
