8c120d73f4fde004e220e3d295ce53a6e0ed27be
Giovane Barcelos <giovanebarcelos@gmail.com>
2025-02-28 20:33:29 -0300

    feat: calculo de um número fatorial com while (R008)

--- src/aula02/R008.java
+++ src/aula02/R008.java
@@ -16,11 +16,28 @@ public class R008 {
         System.out.print("Número: ");
         numero = scanner.nextInt();
 
+        int fatorial = getFatorial(numero);
+        System.out.println("Fatorial For = " + fatorial);
+
+        fatorial = getFatorialWhile(numero);
+        System.out.println("Fatorial While = "+ fatorial);
+    }
+
+    private static int getFatorialWhile(int numero) {
+        int fatorial = 1;
+        int multiplicador = 1;
+        while (multiplicador <=  numero){
+            fatorial = fatorial * multiplicador;
+            multiplicador++;
+        }
+        return fatorial;
+    }
+
+    private static int getFatorial(int numero) {
         int fatorial = 1;
         for (int multiplicador = 1; multiplicador <= numero; multiplicador++){
             fatorial = fatorial * multiplicador;
         }
-
-        System.out.println("Fatorial = "+fatorial);
+        return fatorial;
     }
 }
